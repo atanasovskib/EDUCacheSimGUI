d084c2761760895d439289c37bc6deb69e2b0cee
Blagoj Atanasovski <blagoj.atanasovski@gmail.com>
2014-09-08 10:15:04 +0200

    Drawing successfull

--- src/edu/fcse/cachesim/gui/construction/DrawHere.java
+++ src/edu/fcse/cachesim/gui/construction/DrawHere.java
@@ -1,14 +1,25 @@
 package edu.fcse.cachesim.gui.construction;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 public class DrawHere extends JPanel{
-
+    private String ovoaGoCrtaj;
+    public DrawHere(){
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
+    public Dimension  getPreferredSize() {
+        return new Dimension(250,200);
+    }
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g); 
-        g.drawOval(10, 10, 10, 10);
+        g.drawString(ovoaGoCrtaj, 10, 10);
     }
-    
+    public void setSoDaCrtam(String ovoa){
+        this.ovoaGoCrtaj=ovoa;
+    }
 }
